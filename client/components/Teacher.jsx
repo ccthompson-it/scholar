638cbf53f83379865690cdb11756ebc3c5919304
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-11-17 15:55:18 +1300

    added front end code for user register

--- client/components/Teacher.jsx
+++ client/components/Teacher.jsx
@@ -1,9 +1,35 @@
 import React, { useState, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
-import { isAuthenticated } from 'authenticare/client'
+import { register, isAuthenticated } from 'authenticare/client'
 
 export default function Teacher(props) {
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = () => {
+    register({
+      username: form.username,
+      password: form.password
+    }, {
+      baseUrl: process.env.BASE_API_URL
+    })
+      .then((token) => {
+        if (isAuthenticated()) {
+          props.history.push('/')
+        }
+      })
+  }
+
   return (
     <Fragment>
       <h1>Teacher Page!</h1>
@@ -11,6 +37,10 @@ export default function Teacher(props) {
       <p>
         {isAuthenticated() ? 'You are logged in!' : 'You are not logged in!'}
       </p>
+
+      <input type="text" name="username" value={form.username} onChange={handleChange} />
+      <input type="password" name="password" value={form.password} onChange={handleChange} />
+      <button onClick={handleSubmit}>Register</button>
 
     </Fragment>
   )
